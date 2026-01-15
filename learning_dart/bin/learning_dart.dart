b18aaebf3e303c9ed6cb9497985adaf986e2fb29
Zaw Hlaing Phyo <tro2233zhp@gmail.com>
2026-01-16 02:16:02 +0700

    day 3

--- learning_dart/bin/learning_dart.dart
+++ learning_dart/bin/learning_dart.dart
@@ -12,12 +12,43 @@
 //   print("He has ${hobbies.join(', ')} hobbies.");
 // }
 
-enum TravelStatus { planning, traveling, completed }
+// enum TravelStatus { planning, traveling, completed }
+
+// void main() {
+//   TravelStatus status1 = TravelStatus.planning;
+//   Duration duration1 = Duration(days: 5);
+
+//   TravelStatus status2 = .planning;
+//   Duration duration2 = .new(days: 5);
+// }
+
+// void main() {
+//   // greet();
+//   int result = add(1, 2);
+//   print(result);
+// }
+
+// // void greet() {
+// //   print("Hello");
+// // }
+
+// // int add(int num1, int num2) {
+// //   return num1 + num2;
+// // }
+
+// int add(int num1, int num2) => num1 + num2;
 
 void main() {
-  TravelStatus status1 = TravelStatus.planning;
-  Duration duration1 = Duration(days: 5);
+  profile("Aung Aung", lastName: "Aung", age: 20, height: 5);
+}
 
-  TravelStatus status2 = .planning;
-  
+void profile(
+  String firstName, {
+  String? lastName,
+  required int age,
+  required int height,
+}) {
+  print("Name: $firstName ${lastName ?? ""}");
+  print("Age: $age");
+  print("Height: $height");
 }
